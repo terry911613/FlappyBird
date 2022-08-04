79e1979969f9840d865b155c88513f2fd4ba4265
TaiYiLee <terry911613@gmail.com>
2022-08-04 23:21:46 +0800

    UPDATED: Add Background and Ground

--- FlappyBird/GameViewController.swift
+++ FlappyBird/GameViewController.swift
@@ -16,7 +16,7 @@ class GameViewController: UIViewController {
         
         if let view = self.view as! SKView? {
             // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
+            if let scene = GameplayScene(fileNamed: "GameplayScene") {
                 // Set the scale mode to scale to fit the window
                 scene.scaleMode = .aspectFill
                 
@@ -25,6 +25,7 @@ class GameViewController: UIViewController {
             }
             
             view.ignoresSiblingOrder = true
+            view.showsPhysics = true
             
             view.showsFPS = true
             view.showsNodeCount = true
